472cce8e718f749af4e8531e5f74af101784bdb8
Chris Weed <chris@cjweed.com>
2026-03-22 16:54:57 -0500

    new version

--- images.g.ts
+++ images.g.ts
@@ -911,5 +911,12 @@ ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
         return null;
     })
 
+    helpers._registerFactory("json", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
 }
 // Auto-generated code. Do not edit.
